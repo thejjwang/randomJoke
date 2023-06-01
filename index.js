c40733bc85a50ba3144ddf9311627b697f0b6fdd
thejjwang <jonathanjacobwang@gmail.com>
2023-06-01 15:07:08 -0700

    refactored to change when button is clicked it gives all the jokes

--- index.js
+++ index.js
@@ -18,15 +18,24 @@ function addToList(){
 // input for user to enter joke 
 }
 
-// the actual button that creates random joke   GIVING ERROR!!!!!!!!!!!
-gimmeJoke.addEventListener('click', createJoke);
+// the actual button that creates random joke 
+// gimmeJoke.addEventListener('click', createJoke);
 
-function createJoke(){
-    // creating an array of the values of the obj, which is each joke
-    let values = Object.values(masterJokes);
-    // return random joke
-    let randomValue = values[Math.floor(Math.random() * values.length)];
-    // set the h3 of the joke to the value of the random joke to display the joke
-    h3.innerHTML = randomValue;
+// function createJoke(){
+//     // creating an array of the values of the obj, which is each joke
+//     let values = Object.values(masterJokes);
+//     // return random joke
+//     let randomValue = values[Math.floor(Math.random() * values.length)];
+//     // set the h3 of the joke to the value of the random joke to display the joke
+//     h3.innerHTML = randomValue;
+// }
+// refactor to show master list instead of showing one random joke
+gimmeJoke.addEventListener('click', showList);
+// change size of green box when button is clicked to size it better
+let box = document.getElementById('thisJoke');
+
+function showList(){
+    h3.innerHTML = Object.values(masterJokes);
+    box.style.height = '250px';
+    box.style.width = '250px';
 }
-
